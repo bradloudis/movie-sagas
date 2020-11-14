351ebfc28eccf7274209f24a396950e430e24e07
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-13 18:21:43 -0600

    testing sagas and reducers by rendering movie DB to page in a <pre>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
   // Renders the entire app on the DOM
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'GET_MOVIES',
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Movies!</h1>
-        <Router>
-          {/* ADD PAGES! */}
-        </Router>
+        <Router>{/* ADD PAGES! */}</Router>
+        <pre>{JSON.stringify(this.props.store.movies)}</pre>
         <p>Empty Page</p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStoreToProps = (store) => ({ store });
+export default connect(mapStoreToProps)(App);
